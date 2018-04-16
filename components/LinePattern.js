4ba93cb1c7c93f42c6dcadd33b5062e481c124f0
Dan Burzo <danburzo@gmail.com>
2018-04-16 13:28:14 +0300

    Initial commit

---
+++ components/LinePattern.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LinePattern extends React.Component {
+	render() {
+		return (
+			<pattern 
+				id="linePattern"
+				patternUnits="userSpaceOnUse"
+			>
+				<path d=''/>
+			</pattern>
+		);
+	}
+}
+
+export default LinePattern;
